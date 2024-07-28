880723bd90ca0d9c76c3d4fc555f06d388dd320d
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2024-07-28 23:27:44 +0800

    add docstring add full path in constants

--- src/Snake/constants.py
+++ src/Snake/constants.py
@@ -1,8 +1,15 @@
-DATA_FILE = "data/game.json"
+"""Constants
+"""
+
+from pathlib import Path
+import os
+
+ROOT_PATH = str(Path(__file__).parent) + os.path.sep
+
+DATA_FILE = ROOT_PATH + "data" + os.path.sep + "game.json"
 
 ALIGNMENT = "center"
 FONT_SIZE = 12
 FONT_NAME = "Arial"
 FONT = (FONT_NAME, FONT_SIZE, "normal")
 HIGH_SCORE_FONT = (FONT_NAME, FONT_SIZE, "bold")
-
